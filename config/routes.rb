ecae17c1896d94155a35369024265459b61241aa
Annie Lydens <aelydens@gmail.com>
2015-03-01 22:07:24 -0700

    Set up associations between Lesson and Course & complete migrations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get 'sign-up', to: 'registrations#new'
   post 'sign-up', to: 'registrations#create'
-  resources :classes do
+  get 'sign-in', to: 'authentication#new'
+  post 'sign-in', to: 'authentication#create'
+  get 'sign-out', to: 'authentication#destroy'
+  resources :courses do
     resources :lessons
   end
 end
---
+++ db/schema.rb
@@ -0,0 +1,40 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150302050447) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string  "name"
+    t.integer "capacity"
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.integer "course_id"
+    t.string  "name"
+    t.date    "date"
+  end
+
+  add_index "lessons", ["course_id"], name: "index_lessons_on_course_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_foreign_key "lessons", "courses"
+end
